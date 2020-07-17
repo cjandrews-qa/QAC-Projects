c2d63dfde30ab9d1f61c07b616ac010b5f00b57d
Christopher Andrews <c.andrews4@outlook.com>
2020-07-17 12:03:35 +0100

    Friday Excersise

---
+++ DailyWork/Week2/InterEx.py
@@ -0,0 +1,8 @@
+i =1
+name = []
+while i<=5:
+    name.append(input('What is you name: '))
+    i += 1
+
+for i in name:
+    print(f'{i} is awesome!!')
---
+++ DailyWork/Week2/SolarSystem.py
@@ -0,0 +1,6 @@
+# create list of planets
+planets = ["earth", "jupiter", "neptune", "mars", "saturn", "pluto", "uranus", "venus", "mercury"]
+
+for name in planets: # for each item in the list planets
+    if name != "pluto": # if the item is not "pluto"
+        print(name) # print the item

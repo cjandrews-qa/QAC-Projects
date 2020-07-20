4aac42cd68ac612d7b21cbaa10911239c4663f57
Christopher Andrews <c.andrews4@outlook.com>
2020-07-20 11:34:00 +0100

    Clean up code

--- DailyWork/Week3/Mods/mod1.py
+++ DailyWork/Week3/Mods/mod1.py
@@ -1,4 +1,5 @@
 import random
+
 def dice():
     roll = random.randrange(1, 6)
-    return str(roll)
+    return roll
--- DailyWork/Week3/Mods/mod2.py
+++ DailyWork/Week3/Mods/mod2.py
@@ -3,7 +3,7 @@ import mod1
 def roll2():
     one = mod1.dice()
     two = mod1.dice()
-    throw = (f'Dice 1 = {one} \nDice 2 = {two}\nValue of Dice = {int(one)+int(two)}')
+    throw = (f'Dice 1 = {one} \nDice 2 = {two}\nValue of Dice = {one+two}')
     return throw
 
 print(roll2())

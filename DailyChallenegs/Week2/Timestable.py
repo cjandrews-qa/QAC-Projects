e49d979c0010195330b7ee0838c16b01666be978
Christopher Andrews <c.andrews4@outlook.com>
2020-07-20 10:15:01 +0100

    adding function into the challenge

--- DailyChallenegs/Week2/Timestable.py
+++ DailyChallenegs/Week2/Timestable.py
@@ -1,9 +1,11 @@
 n=int(input('Enter a Number: '))
-for hori in range(1,n+1):
-    for vert in range(2,n+1):
-        print(hori*vert, end="\t")
-    print()
-
+def timet(n):
+    line = ""
+    for hori in range(1,n+1):
+        for vert in range(2,n+1):
+            line = line + (hori*vert) + "\t"
+        line += "\n"
+timet(n)
         
 
 
